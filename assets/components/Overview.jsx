ec060dc95c01f1de3756fcea8fa96f2c30885750
Scott Dover <scott.dover@researchsquare.com>
2020-07-11 18:18:56 -0400

    Update bio pic / add badges

--- assets/components/Overview.jsx
+++ assets/components/Overview.jsx
@@ -1,8 +1,13 @@
+import './overview.scss';
 import Panel from './Panel';
 import React from 'react';
 
+import avBadge from './av-badge.png';
+import superLawyer from './super-lawyer.png';
+import superLawyer2x from './super-lawyer@2x.png';
+
 const Overview = () => (
-    <Panel label="home">
+    <Panel label="home" className="overview">
         <p className="intro">
             The Law Offices of R. Scott Dover provide general litigation services to individuals in Pickens, Oconee,
             Greenville, Anderson, and Spartanburg Counties in the Upstate of South Carolina. Scott Dover has spent more
@@ -10,6 +15,10 @@ const Overview = () => (
             America,&rdquo; &ldquo;South Carolina Super Lawyers,&rdquo; and has received a &ldquo;AV Preeminent&rdquo;
             rating by Martindale-Hubbell.
         </p>
+        <div className="badges">
+            <img src={superLawyer} srcset={`${superLawyer2x} 2x`} alt="South Carolina Super Lawyers" />
+            <img src={avBadge} alt="AV Preeminent" />
+        </div>
     </Panel>
 );
 
